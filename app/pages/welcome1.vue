31333ffe87922f67ddf397c711fd1d745bbf58ad
Sarah <sarah.3anany@gmail.com>
2025-09-22 13:53:50 +0300

    dark

--- app/pages/welcome1.vue
+++ app/pages/welcome1.vue
@@ -9,7 +9,7 @@ const emit = defineEmits(['next'])
     <div class="flex flex-col items-center justify-center w-screen h-screen px-4 text-center mt-1/5">
         <img :src="page1" alt="welcome" class="w-4/5 h-1/3 mx-1/5" />
         <div class="gap-4 xs:w-3/4 lg:w-1/2">
-            <h3 class="font-georgia text-secondary-500 lg:text-2xl xs:text-sm">Book Your Appointment Easily</h3>
+            <h3 class="font-georgia text-secondary-500 lg:text-2xl xs:text-sm dark:text-secondary-500">Book Your Appointment Easily</h3>
             <p class="text-neutral-700 font-montserratMedium lg:text-base xs:text-2xs leading-relaxed mt-5">
                 Choose your preferred doctor, pick a suitable time, 
                 and confirm your visit in just a few taps. No calls, 
