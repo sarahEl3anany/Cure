5b1fe4858a10fa7160cc8d169164bf3efb9f9afa
Sarah <sarah.3anany@gmail.com>
2025-09-30 16:58:42 +0300

    home head

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
+import userAvatar from '@/assets/images/home/defaultUser.svg'
+let avatar = process.client ? sessionStorage.getItem('avatar') : undefined;
+if(!avatar) {
+    avatar = useCookie('avatar').value ? useCookie('avatar').value : undefined
+    if(!avatar) {
+        avatar = userAvatar
+    }
+}
 </script>
 <template>
-    home
+    <div class="flex flex-col items-center justify-center px-4 text-center">
+        <div class="header h-10 justify-between" >
+            <div>
+                <img :src="avatar" alt="Login" class="w-10 h-10 mx-1/5" />
+            </div>
+            <div>
+
+            </div>
+            <div>
+                <div>
+                    Welcome back, {{ userName }}
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
