4e9fb0020dcef9f0886044196ef39b2f9045ca8f
Sarah <sarah.3anany@gmail.com>
2025-10-03 11:11:39 +0300

    plg

---
+++ app/plugins/facebook.client.ts
@@ -0,0 +1,28 @@
+export default defineNuxtPlugin(() => {
+  return new Promise<void>((resolve) => {
+    if ((window as any).FB) {
+      resolve();
+      return;
+    }
+
+    (window as any).fbAsyncInit = function () {
+      (window as any).FB.init({
+        appId: useRuntimeConfig().public.facebookAppId,
+        cookie: true,
+        xfbml: true,
+        version: 'v18.0',
+      });
+      resolve();
+    };
+
+    (function (d, s, id) {
+      let js: HTMLScriptElement;
+      const fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s) as HTMLScriptElement;
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode!.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  });
+});
